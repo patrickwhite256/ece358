8a35b902ccd7d7ca7c8ce1562a5c0764ff2caae5
jmizzoni <jpmizzoni@gmail.com>
2016-07-19 13:08:13 -0400

    making global vars static

--- nottcp/rcs_socket.cpp
+++ nottcp/rcs_socket.cpp
@@ -1,28 +1,30 @@
 #include "ucp.h"
 #include "rcs_socket.h"
 
+int RCSSocket::g_rcs_sock_counter = 0;
+
 int create_rcs_sock() {
     RCSSocket rcs_sock(ucpSocket());
 
-    int id = g_rcs_sock_counter;
-    g_rcs_sock_counter++;
+    int id = RCSSocket::g_rcs_sock_counter;
+    RCSSocket::g_rcs_sock_counter++;
 
     rcs_sock.id = id;
-    g_rcs_sockets.insert(std::pair<int, RCSSocket>(id, rcs_sock));
+    RCSSocket::g_rcs_sockets.insert(std::pair<int, RCSSocket>(id, rcs_sock));
 
     return id;
 }
 
 int close_rcs_sock(int sockfd) {
-    int result = ucpClose(g_rcs_sockets.find(sockfd)->second.ucp_sockfd);
+    int result = ucpClose(RCSSocket::g_rcs_sockets.find(sockfd)->second.ucp_sockfd);
 
     if (result == 0) {
-        g_rcs_sockets.erase(sockfd);
+        RCSSocket::g_rcs_sockets.erase(sockfd);
     }
 
     return result;
 }
 
 RCSSocket get_rcs_sock(int sockfd) {
-    return g_rcs_sockets.find(sockfd)->second;
+    return RCSSocket::g_rcs_sockets.find(sockfd)->second;
 }
--- nottcp/rcs_socket.h
+++ nottcp/rcs_socket.h
@@ -4,14 +4,14 @@
 #include <map>
 
 struct RCSSocket {
+    static int g_rcs_sock_counter;
+    static std::map<int, RCSSocket> g_rcs_sockets;
+
     int id;
     int ucp_sockfd;
 
     RCSSocket(int sockfd) : ucp_sockfd(sockfd) {}
 };
-
-int g_rcs_sock_counter = 0;
-std::map<int, RCSSocket> g_rcs_sockets;
 
 int create_rcs_sock();
 int close_rcs_sock(int sockfd);
