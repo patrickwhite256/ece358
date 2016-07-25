d869ab0d0e3ceb05c5ba3ca0b675223c8bc28d08
Patrick White <patrickwhite.256@gmail.com>
2016-07-25 13:06:32 -0400

    ack seq#, port#, unsigned best signed

--- nottcp/src/message.h
+++ nottcp/src/message.h
@@ -3,17 +3,20 @@
 
 #define FLAG_ACK 0x01 // 0000 0001 -- acknowledgement of a message
 #define FLAG_SYN 0x02 // 0000 0010 -- synchronize request
-#define FLAG_SQN 0x80 // 1000 0000 -- sequence number
+#define FLAG_SQN 0x40 // 0100 0000 -- sequence number
+#define FLAG_AKN 0x80 // 1000 0000 -- ack sequence number
 
 #define CHECKSUM_SIZE   2 // 16 bits
 #define SIZE_SIZE       2 // 16 bits
-#define FLAGS_SIZE      1 // 8 bits (probably unnecessary, but easier to work with if it's just a char)
+#define FLAGS_SIZE      1 // 8 bits
+#define PNO_SIZE        1 // 8 bits
 
-#define HEADER_SIZE (CHECKSUM_SIZE + SIZE_SIZE + FLAGS_SIZE)
+#define HEADER_SIZE (CHECKSUM_SIZE + SIZE_SIZE + FLAGS_SIZE + PNO_SIZE)
 
 #define CHECKSUM_OFFSET 0
 #define SIZE_OFFSET     (CHECKSUM_OFFSET + CHECKSUM_SIZE)
 #define FLAGS_OFFSET    (SIZE_OFFSET     + SIZE_SIZE)
+#define PNO_OFFSET      (FLAGS_OFFSET    + FLAGS_SIZE)
 
 #include <cstdint>
 
@@ -25,26 +28,27 @@
  * |-------------------------|
  * |   16-bit message size   |
  * |-------------------------|
- * | 8 flag bits |
- *  -------------
+ * | 8 flag bits | "port" #  |
+ *  -------------------------
  */
 
 struct Message {
     uint16_t checksum;
     uint8_t flags;
+    uint8_t portno;
     uint8_t *header;
     char *content;
     uint16_t size;
 
-    Message(const char *msg_content, uint16_t content_size, uint8_t flags);
+    Message(const char *msg_content, uint16_t content_size, uint8_t flags, uint8_t portno);
 
     uint16_t get_content_size();
     void set_header();
     void set_checksum();
-    char *serialize();
+    uint8_t *serialize();
     bool validate();
 };
 
-Message deserialize(const char *buf);
+Message deserialize(const uint8_t *buf);
 
 #endif
